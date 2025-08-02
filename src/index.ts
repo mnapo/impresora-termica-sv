032104e5083a2edf235891b837bc7b2629c557bd
mnapo <napolimatiase@gmail.com>
2025-08-02 16:08:19 -0300

    rollback to unprotected until testing phase is finished

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
-import https from 'https'
-import fs from 'fs'
 import { app } from './app'
 import { logger } from './logger'
 
 const port = app.get('port')
 const host = app.get('host')
-logger.info('helloooo')
+
 process.on('unhandledRejection', reason => logger.error('Unhandled Rejection %O', reason))
 
-const server = https
-  .createServer(
-    {
-      key: fs.readFileSync('server.key'),
-      cert: fs.readFileSync('server.csr')
-    },
-    app.callback()
-  )
-  .listen(443)
-
-app.setup(server)
-
-server.on('listening', () => logger.info('Feathers application started'))
+app.listen(port).then(() => {
+  logger.info(`Feathers app listening on http://${host}:${port}`)
+})
