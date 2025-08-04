011f9b96702e0960e970ef7d98d539bdefbc4ebd
mnapo <napolimatiase@gmail.com>
2025-08-04 20:46:40 -0300

    add products service's schema

---
+++ src/services/products/products.schema.ts
@@ -0,0 +1,57 @@
+// // For more information about this file see https://dove.feathersjs.com/guides/cli/service.schemas.html
+import { resolve } from '@feathersjs/schema'
+import { Type, getValidator, querySyntax } from '@feathersjs/typebox'
+import type { Static } from '@feathersjs/typebox'
+
+import type { HookContext } from '../../declarations'
+import { dataValidator, queryValidator } from '../../validators'
+import type { ProductsService } from './products.class'
+
+// Main data model schema
+export const productsSchema = Type.Object(
+  {
+  id: Type.Number(),
+  userId: Type.Number(),
+  name: Type.String({ minLength: 1 }),
+  description: Type.Optional(Type.String()),
+  price: Type.Number({ minimum: 0 }),
+  createdAt: Type.Optional(Type.String({ format: 'date-time' })),
+  updatedAt: Type.Optional(Type.String({ format: 'date-time' }))
+  },
+  { $id: 'Products', additionalProperties: false }
+)
+export type Products = Static<typeof productsSchema>
+export const productsValidator = getValidator(productsSchema, dataValidator)
+export const productsResolver = resolve<Products, HookContext<ProductsService>>({})
+
+export const productsExternalResolver = resolve<Products, HookContext<ProductsService>>({})
+
+// Schema for creating new entries
+export const productsDataSchema = Type.Pick(productsSchema, ['name', 'description', 'price'], {
+  $id: 'ProductsData'
+})
+export type ProductsData = Static<typeof productsDataSchema>
+export const productsDataValidator = getValidator(productsDataSchema, dataValidator)
+export const productsDataResolver = resolve<Products, HookContext<ProductsService>>({})
+
+// Schema for updating existing entries
+export const productsPatchSchema = Type.Partial(productsSchema, {
+  $id: 'ProductsPatch'
+})
+export type ProductsPatch = Static<typeof productsPatchSchema>
+export const productsPatchValidator = getValidator(productsPatchSchema, dataValidator)
+export const productsPatchResolver = resolve<Products, HookContext<ProductsService>>({})
+
+// Schema for allowed query properties
+export const productsQueryProperties = Type.Pick(productsSchema, ['name', 'description', 'price'])
+export const productsQuerySchema = Type.Intersect(
+  [
+    querySyntax(productsQueryProperties),
+    // Add additional query properties here
+    Type.Object({}, { additionalProperties: false })
+  ],
+  { additionalProperties: false }
+)
+export type ProductsQuery = Static<typeof productsQuerySchema>
+export const productsQueryValidator = getValidator(productsQuerySchema, queryValidator)
+export const productsQueryResolver = resolve<ProductsQuery, HookContext<ProductsService>>({})
