1d92cdfe57080ad1f0cd2d6b41ab3ca6731aeb76
mnapo <napolimatiase@gmail.com>
2025-08-05 03:32:39 -0300

    block supperposition

--- migrations/20250805033149_populate.ts
+++ migrations/20250805033149_populate.ts
@@ -2,11 +2,7 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-    knex('users').insert({ email: '1', password: '1' })
-    .returning(['id', 'email', 'password'])
-    .then(rows => {
-    console.log('Inserted user:', rows[0]);
-  })
+
 }
 
 
