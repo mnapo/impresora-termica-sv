2f442d4d1e562d64ba6b75c985e05554e4b8d08d
mnapo <napolimatiase@gmail.com>
2025-08-05 15:25:09 -0300

    set testing credentials

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,20 @@ const host = app.get('host')
 process.on('unhandledRejection', reason => logger.error('Unhandled Rejection %O', reason))
 
 app.listen(port).then(() => {
-  logger.info(`Feathers app listening on http://${host}:${port}`)
   const superUser = {
-      email: 'admin',
-      password: 'admin',
+    email: 'admin',
+    password: 'admin'
   };
   app.service('users').create(superUser).then(user => {
-      console.log('Admin user created:', user)
+    console.log('Admin user created:', user)
   }).catch(error => {
+    console.error('Error creating admin user:', error)
+  }).then(() => {
+    app.service('users').patch(1, { role: 'admin' }).then(() => {
+      console.log('Admin role assigned to user with ID 1')
+    }).catch(error => {
       console.error('Error creating admin user:', error)
+    })
   })
+  logger.info(`Feathers app listening on http://${host}:${port}`)
 })
