21f344f2da94e032909247aed92767112f0c088d
mnapo <napolimatiase@gmail.com>
2025-08-05 01:30:28 -0300

    show email row

--- migrations/20250805033149_populate.ts
+++ migrations/20250805033149_populate.ts
@@ -3,12 +3,10 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     knex('users').insert({ email: '1', password: '1' })
-    .then(function(result) {
-    console.log('Inserted data:', result); // result typically contains the ID of the inserted row
-    })
-    .catch(function(error) {
-    console.error('Error inserting data:', error);
-    });
+    .returning(['id', 'email', 'password'])
+    .then(rows => {
+    console.log('Inserted user:', rows[0]);
+  })
 }
 
 
