1d4176bc58c219fce5bc3c74947554158c062787
mnapo <napolimatiase@gmail.com>
2025-08-08 18:45:12 -0300

    avoid dropTable on disconnect at testing phase

--- migrations/20250804220013_products.ts
+++ migrations/20250804220013_products.ts
@@ -15,5 +15,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('products')
+  //await knex.schema.dropTable('products')
 }
