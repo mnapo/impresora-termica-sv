0598a4cbbf0a3f4c7442d2960fb7f3d21361ebdb
mnapo <napolimatiase@gmail.com>
2025-08-30 00:41:17 -0300

    replace createSuperUser with createUser and createCondIvaTypes with createCondIvaType

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,65 @@
 import { app } from './app'
 import { logger } from './logger'
 
+type User = {
+  email: string
+  password: string
+  role: string
+  firstName: string
+  lastName: string
+  cuit: string
+  cbu: string
+  alias: string
+  condIvaTypeId: number
+}
+
+export const createCondIvaType = async (condIvaType: string) => {
+  try {
+    await app.service('cond-iva-types').create({name: condIvaType})
+    logger.info(`\tcondIvaType ${condIvaType} created`)
+  } catch (error) {
+    logger.error('\tError creating cond-iva-types:', error)
+  }
+}
+
+const createUser = async (id: number, user: User) => {
+  const userRecord = await app.service('users').find({query:{email: user.email}})
+  logger.info(`\tAdding user:  ${user.email}`)
+  userRecord.data.length>0?logger.info("\tUser already exists"):
+  app.service('users').create({email: user.email, password: user.password}).then(createdRecord => {
+    logger.info( `\tUser created:`, createdRecord)
+    app.service('users').patch( id, 
+    {
+      role: user.role,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      cuit: user.cuit,
+      cbu: user.cbu,
+      alias: user.alias,
+      condIvaTypeId: user.condIvaTypeId
+    }).then(() => {
+      console.log(`\tProperties asiggned tu user: ${user.email}`)
+    }).catch(error => {
+      console.error('\tError updating user:', error)
+    })
+  }).catch(error => {
+    console.error(`\tError creating user: ${user.email}`, error)
+  })
+}
+
 const port = app.get('port')
 const host = app.get('host')
 
-const createCondIvaTypes = async () => {
-  try {
-    await app.service('cond-iva-types').find()
-    await app.service('cond-iva-types').create({ name: 'Responsable Inscripto' });
-    await app.service('cond-iva-types').create({ name: 'IVA Exento' });
-    await app.service('cond-iva-types').create({ name: 'Monotributo' });
-    logger.info('\tcondIvaTypes created');
-  } catch (error) {
-    logger.error('\tError creating cond-iva-types:', error);
-  }
-};
-
-const createSuperUser = async () => {
-  const superUser = {email: 'admin', password: 'admin'};
-  app.service('users').create(superUser).then(user => {
-    logger.info('\tSuper user created:', user)
-  }).catch(error => {
-    console.error('\tError creating super user:', error)
-  }).then(() => {
-    app.service('users').patch(1, { role: 'admin', firstName: 'Admin', lastName: 'Admin' }).then(() => {
-      console.log('\tAdmin role assigned to super user')
-    }).catch(error => {
-      console.error('\tError updating super user:', error)
-    })
-  });
-};
 const populate = async () => {
-  const superUserRecord = await app.service('users').find({query:{email: 'admin'}});
-  superUserRecord.data.length>0?logger.info("\tSuper user already exists"):createSuperUser();
   const firstCondIvaTypeRecord = await app.service('cond-iva-types').find({query:{name: 'Responsable Inscripto'}});
-  firstCondIvaTypeRecord.data.length>0?logger.info("\tcondIvaTypes already exist"):createCondIvaTypes();
+  if (firstCondIvaTypeRecord.data.length>0)
+    logger.info("\tcondIvaTypes already exist")
+  else {
+    createCondIvaType('Responsable Inscripto')
+    createCondIvaType('Monotributo')
+    createCondIvaType('Iva Exento')
+  }
+  //createUser(1, { email: 'your@email.com', password: 'your-pw', role: 'admin', firstName: 'your-first-name', lastName: 'your-last-name', cuit: "", cbu: '', alias: '', condIvaTypeId: 1} );
 };
 
 process.on('unhandledRejection', reason => logger.error('Unhandled Rejection %O', reason))
