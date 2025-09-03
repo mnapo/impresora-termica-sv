10d8c179e834141d6fb22812c1d2bd4b9cec7c46
mnapo <napolimatiase@gmail.com>
2025-09-03 20:26:58 -0300

    fix validateUnique hook

--- src/hooks/validate-unique.ts
+++ src/hooks/validate-unique.ts
@@ -1,27 +1,29 @@
-// src/hooks/validateUnique.ts
-import { BadRequest } from '@feathersjs/errors'
 import type { HookContext } from '@feathersjs/feathers'
+import { BadRequest } from '@feathersjs/errors'
 
 export const validateUnique = (field: string) => {
   return async (context: HookContext) => {
     const { data, id, service, params } = context
-    // patch: if no data for the field, skip
-    if (!data?.[field] || !data?.userId) return context
+
+    const userId = params.user?.id
+
+    if (!data?.[field] || !userId) {
+      return context
+    }
 
     const query: any = {
       [field]: data[field],
-      userId: data.userId,
+      userId,
       $limit: 1
     }
 
-    // update/patch: exclude current record
     if (id) query.id = { $ne: id }
 
     const existing: any = await service.find({ ...params, query })
-
     const count = Array.isArray(existing) ? existing.length : existing.total ?? 0
+
     if (count > 0) {
-      throw new BadRequest(`Error: "${field}" already exists.`)
+      throw new BadRequest(`Record already exist`)
     }
 
     return context
